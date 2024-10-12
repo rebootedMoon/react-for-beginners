66f125b0d3e7b1ee72027454bb5c1508c870564f
rebootedCoder2 <oborimoon@gmail.com>
2024-10-13 06:52:37 +0900

    toDolist

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import { useState } from "react";
 import styles from "./App.module.css";
 import styles_btn from "./Button.module.css";
 function App() {
-  const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter((prev) => prev + 1);
-  console.log("render");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo == "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      <h1 className={styles.title}> {counter}</h1>
-      <button className={styles_btn.btn} onClick={onClick}>
-        Click Me
-      </button>
+      <h1> My ToDos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={toDo}
+          onChange={onChange}
+          placeholder="Write your to do ..."
+        />
+        <button>Add to do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}> {item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
